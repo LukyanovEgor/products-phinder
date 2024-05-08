fa6338692b35f2117478949e6ccfc93dd6e6b3ab
leha-pasechnikov <pasechnikov.leha@inbox.ru>
2024-05-08 20:32:15 +0300

    feat(telegram): add TelegramAPI template. A telegram bot has been added that responds to user messages.

--- telegram.py
+++ telegram.py
@@ -1,5 +1,10 @@
-<<<<<<< HEAD
-print('product-phinder')
-=======
-print('product-phinder')
->>>>>>> 52f7c76 (feat(telegram): add base template)
+import telebot
+bot = telebot.TeleBot('6723013452:AAGxlvSp5Deemgxuqbk0Cv4u9xVE2dBQhlU')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+  if message.text == "Привет":
+    bot.send_message(message.from_user.id, "Привет!!!")
+
+bot.polling(none_stop=True, interval=0)
+print('FW')
